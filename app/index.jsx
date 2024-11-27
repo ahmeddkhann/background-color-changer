fcb3beedb47022c9f598e7ece760e89f8ec6cbda
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-11-27 18:58:53 +0500

    logic part completed

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, {useState} from 'react'
+import { StatusBar } from 'expo-status-bar';
 
 const Index = () => {
+
+const [NewbackgroundColor, setNewBackgroundColor] = useState("#ffffff");
+
+const backgroundColorChanger = () => {
+  const hexRange = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += hexRange[Math.floor(Math.random() * 16)]
+    
+  }
+  setNewBackgroundColor(color);
+}
+  
   return (
-    <View>
-      <Text>Index</Text>
-    </View>
+    <>
+   <StatusBar backgroundColor={NewbackgroundColor}/>
+   <View style= {[{backgroundColor: setNewBackgroundColor}]}>
+    <TouchableOpacity onPress={backgroundColorChanger}>
+      <View>
+        <Text>
+          Press me
+        </Text>
+      </View>
+    </TouchableOpacity>
+   </View>
+    </>
   )
 }
 
